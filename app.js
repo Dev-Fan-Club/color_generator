d8a9856d51e00ff5bdc2f5c1eda4a26efdc3b9ed
nobijoy <nobijoy1446@gmail.com>
2022-08-25 23:01:48 +0600

    not done yet

--- app.js
+++ app.js
@@ -9,5 +9,32 @@ let outputs = document.getElementById("outputs");
     outputs.style.backgroundColor =
     'rgb(' + red + ',' + green + ',' + blue + ')';
 
-    document.getElementById("output").innerHTML =  'rgb(' + red + ',' + green + ',' + blue + ')' ;
+    document.getElementById("output").innerHTML =  RGBToHex(red ,green, blue) ;
+    //'rgb(' + red + ',' + green + ',' + blue + ')'
     }
+
+    function setDefault(){
+
+    }
+
+    function copyToClipboard() {
+        var copyText = document.getElementById("output");
+        console.log(copyText);
+        copyText.select();
+        document.execCommand("copy");
+         
+     }
+     function RGBToHex(r,g,b) {
+        r = r.toString(16);
+        g = g.toString(16);
+        b = b.toString(16);
+      
+        if (r.length == 1)
+          r = "0" + r;
+        if (g.length == 1)
+          g = "0" + g;
+        if (b.length == 1)
+          b = "0" + b;
+      
+        return "#" + r + g + b;
+      }
